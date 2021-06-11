1bdd0bbdf5b858f667709661f068d9f71f2f88b5
V <vtzuang@gmail.com>
2021-06-11 11:37:47 -0700

    Added users scaffolding

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,37 @@ class ApplicationController < ActionController::Base
         render html: "hello, world!"
     end
 
+    def index
+
+
+    end
+
+    def show
+
+
+    end
+
+    def new
+
+
+    end
+
+    def edit
+
+    end
+
+    def create
+
+    end
+
+    def update
+
+
+    end
+
+    def destroy
+
+
+    end
+
 end
